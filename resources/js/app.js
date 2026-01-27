35fce38a567b0ce67bb64cc31a5b8162ecfa4b6f
husnainNexius <husnainNexius@gmail.com>
2026-01-27 21:15:02 +0500

    feat/UserModule added RegiisterApi. LoginApi, ViewUserDetail api added tests for each api vue configuration added welcome blade view updated

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+// Import components manually
+import ExampleComponent from './Modules/Example/Components/ExampleComponent.vue';
+import UserCard from './Modules/User/Components/UserCard.vue';
+
+const app = createApp({});
+
+// Register components manually
+app.component('ExampleComponent', ExampleComponent);
+app.component('UserCard', UserCard);
+
+app.mount('#app');
